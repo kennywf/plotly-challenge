5dfafb3e482c1b39a7bc2442dfb742d5b36e7d1c
kennywf <63686149+kennywf@users.noreply.github.com>
2020-08-27 14:23:55 -0400

    upload

--- static/js/app.js
+++ static/js/app.js
@@ -1,70 +1,101 @@
-const url = '/data/samples.json';
-
-function biodiversityData (sample) {
+// Create url path to data file
+var url = "data/samples.json"
 
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
-    console.log(data);
-  });
-}
+  console.log(data);
+});
+
+// Create charts
+function charts(sample) {    
+  d3.json(url).then((data) => {
+  var samples = data.samples;
+// Filter by id for each sample within samples   
+  var filteredSamples = samples.filter(sampleSelection => sampleSelection.id == sample);
+  var result = filteredSamples[0];
 
 // Build horizontal bar chart with top 10 OTU
-function bar(sample) {
-  d3.json(url).then(function(data) {
-    // Slice up to 10 samples
-    var hoverText = samples.otu_labels.slice(0,10);
-    var trace1 = {
-      x: samples.otu_ids.slice(0,10),
-      y: samples.sample_values.slice(0,10),
-      orientation: h,
-      marker: {
-        color: 'rgba(55,128,191,0.6)'},
-      type: "bar"
-    }
+// Use .reverse() to make the first element become the last
+  var trace1 = {
+      x: result.sample_values.slice(0,10).reverse(),
+      y: result.otu_ids.slice(0,10).map(otu_id => `OTU ${otu_id}`).reverse(),
+      text: result.otu_labels.slice(0,10).reverse(),
+      type: "bar",
+// This line creates a horizontal bar chart 
+      orientation: "h"
+  };
+  var data = [trace1];
+  var layout = {
+      title: "Top Ten OTUs for Test Subject ID: " +sample,
+      hovermode: 'closest',
+  };
+  Plotly.newPlot("bar", data, layout);  
 
-var data = [trace1];
-
-var layout = {
-  title: "Top 10 OTU's"
-};
-
-Plotly.newPlot("plot", data, layout);
-});
-}
-
-//Build the bubble chart 
-function bubble(sample) {
-  d3.json(url).then(function(data) {
-    var trace2 = {
-      x: data.samples.otu_ids,
-      y: data.samples.sample_values,
+// Create bubble chart that displays each sample 
+  var trace1 = {
+      x: result.otu_ids,
+      y: result.sample_values,
+      text: result.otu_labels,
       mode: 'markers',
       marker: {
-        size: data.samples.sample_values,
-        color: data.samples.otu_ids},
-      text: data.samples.otu_labels
-};
-
-var data = [trace2]
-
-var layout = {
-  title: "OTU_IDs"
-};
-
-Plotly.newPlot('myDiv', data, layout);
-});
+      size: result.sample_values,
+      color: result.otu_ids,
+      colorscale:"Earth"
+      }
+  };
+  var data = [trace1];
+  var layout = {
+      title: 'Bacteria Cultures per Sample',
+      hovermode: 'closest',
+      xaxis: {title:"Operational Taxonomic Unit (OTU) ID " + sample},
+  };
+  Plotly.newPlot('bubble', data, layout); 
+  }); 
 }
 
 // Create the metadata table
-function createTable(sample) {
-d3.json(url).then(function(data){
-  const metadata_sample = d3.select("#sample-metadata")
-// Clear any existing metadata
-  metadata_sample.html("");
-// Add key and value pairs
-  Object.entries(sample_data).forEach(function ([key,value]){
-    const row = metadata_sample.append("p");
-    row.text(`${key}: ${value}`);
+function metadataTable(sample) {
+  d3.json(url).then((data) => {
+      var metadata = data.metadata;
+// Filter by id for each sample within metadata
+      var filteredSamples = metadata.filter(sampleSelection => sampleSelection.id == sample);
+      var result = filteredSamples[0];
+// Use d3 to select the table section "sample-metadata"
+      var tableSection = d3.select("#sample-metadata");
+// Clear existing data after clicking on a different sample
+      tableSection.html("");
+// Add key-value pairs 
+      Object.entries(result).forEach(([key, value]) => {
+// Append the rows
+          tableSection.append("h6").text(`${key}: ${value}`)
+      })
   });
-});
 }
+
+function optionChanged(newSample) {
+// Refreshes data when clicking on a different sample
+  charts(newSample);
+  metadataTable(newSample);
+}
+
+function init() {
+// Use d3 to select the dropdown element in the html
+  var dropdown = d3.select("#selDataset");
+// Add all the ID's to the dropdown
+    d3.json(url).then((data) => {
+      var subjectIds = data.names;
+      subjectIds.forEach((id) => {
+        dropdown
+        .append("option")
+        .text(id)
+        .property("value", id);
+      });
+// Use the first ID for initial plots and metadata section
+    const firstSubject = subjectIds[0];
+    charts(firstSubject);
+    metadataTable(firstSubject);
+  });
+}
+
+// Initialize the dashboard
+init();
